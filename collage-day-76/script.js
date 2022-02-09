f6a74a37472b117b901dde63e5ee17ccd3d2844e
Debora Ermel <debbyermel@gmail.com>
2022-02-09 13:35:07 -0600

    Day 76

--- collage-day-76/script.js
+++ collage-day-76/script.js
@@ -1,6 +1,5 @@
 const rellax = new Rellax('.rellax');
 
-// grabs the media query
 const motionMediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
 
 // if the user prefers reduced motion, disable parallax
